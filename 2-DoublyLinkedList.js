2718056906e790bd334dc8806545c41758431975
APdrone <amanpatwalfor@gmail.com>
2024-02-28 14:38:06 +0530

    adding doubly linkedlist part2

--- 2-DoublyLinkedList.js
+++ 2-DoublyLinkedList.js
@@ -41,8 +41,68 @@ class DoublyLinkedList {
     }
     this.length--;
   }
+  shift() {
+    if (!this.head) {
+      return undefined;
+    }
+    let oldHead = this.head;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+    this.length--;
+    return oldHead;
+  }
+  unshift(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    let oldHead = this.head;
+    this.head = newNode;
+    this.head.next = oldHead;
+    oldHead.prev = newNode;
+    this.length++;
+    return this;
+  }
+  get(index) {
+    if (index >= this.length || index < 0) {
+      return undefined;
+    }
+    console.log(Math.floor(this.length / 2));
+    console.log(Math.round(this.length / 2) - index <= 0);
+    let nextNode;
+    if (index <= this.length / 2) {
+      console.log('head render');
+      nextNode = this.head;
+
+      for (let i = 0; i < index; i++) {
+        nextNode = nextNode.next;
+      }
+    } else {
+      console.log('tail render');
+      nextNode = this.tail;
+
+      for (let i = 0; i < this.length - index - 1; i++) {
+        nextNode = nextNode.prev;
+      }
+    }
+    return nextNode;
+  }
 }
 
 const list = new DoublyLinkedList();
+
+list.push('yes');
+list.push('hello');
+list.push('are');
+list.push('you');
+list.push('there!');
 
 console.log(list);
