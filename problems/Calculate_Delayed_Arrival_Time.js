c91a284ef45d1b810b796e58dca0b705c4d37c53
APdrone <amanpatwalfor@gmail.com>
2024-05-09 22:35:13 +0530

    adding lp

---
+++ problems/Calculate_Delayed_Arrival_Time.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} arrivalTime
+ * @param {number} delayedTime
+ * @return {number}
+ */
+var findDelayedArrivalTime = function (arrivalTime, delayedTime) {
+  return (arrivalTime + delayedTime) % 24;
+};
+
+console.log(findDelayedArrivalTime(15, 5));
+console.log(findDelayedArrivalTime(13, 11));
+console.log(findDelayedArrivalTime(24, 24));
